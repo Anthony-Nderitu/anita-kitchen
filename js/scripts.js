1d36ea9e651848a18ec24d570d43765dd3470a36
Anthony-Nderitu <anderitu1997@yahoo.com>
2018-04-27 18:04:51 +0300

    Add alert when subscribe.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,10 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+$(document).ready(function() {
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var popup = $("#email").val();
+    alert(popup + " " + "has been successfully added to our email list. Thank you.");
+  });
+});
